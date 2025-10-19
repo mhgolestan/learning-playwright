fbd50ff60edc6fc21e8ee09a0af0e3561954301c
mhgolestan <golestan1369@gmail.com>
2025-10-19 20:30:18 +0200

    login page object added

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -1,19 +1,24 @@
 import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../pages/login/LoginPage";
 
-test("login test", async ({ page }) => {
-  await page.goto("https://practicesoftwaretesting.com/");
-  await page.getByTestId("nav-sign-in").click();
-  
-  await page
-    .getByTestId("email")
-    .fill("customer@practicesoftwaretesting.com");
-  
-  await page.getByTestId("password").fill("welcome01");
-  await page.getByTestId("login-submit").click();
-  await expect(page.getByTestId("nav-menu")).toContainText(
-    "Jane Doe"
-  );
-  await expect(page.getByTestId("page-title")).toContainText(
-    "My account"
-  );
+test.describe("Login page test", () => {
+  test("login test without page object", async ({ page }) => {
+    await page.goto(process.env.URL);
+    await page.getByTestId("nav-sign-in").click();
+
+    await page
+      .getByTestId("email")
+      .fill("customer@practicesoftwaretesting.com");
+
+    await page.getByTestId("password").fill("welcome01");
+    await page.getByTestId("login-submit").click();
+    await expect(page.getByTestId("nav-menu")).toContainText("Jane Doe");
+    await expect(page.getByTestId("page-title")).toContainText("My account");
+  });
+
+  test("login with page object", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    loginPage.login("customer@practicesoftwaretesting.com", "welcome01");
+  });
 });
